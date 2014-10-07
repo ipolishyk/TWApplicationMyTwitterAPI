76c5ae23192453c1158095edb196cee67fe16e3f
Maksim Polishyk <Polishyk@iMac-Maksim.local>
2014-10-07 17:33:49 +0300

    Initial Commit

---
+++ TWApplicationMyTwitterAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TWApplicationMyTwitterAPI
+//
+//  Created by Maksim Polishyk on 07.10.14.
+//  Copyright (c) 2014 Maksim Polishyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TWApplicationMyTwitterAPITests/TWApplicationMyTwitterAPITests.m
@@ -0,0 +1,40 @@
+//
+//  TWApplicationMyTwitterAPITests.m
+//  TWApplicationMyTwitterAPITests
+//
+//  Created by Maksim Polishyk on 07.10.14.
+//  Copyright (c) 2014 Maksim Polishyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TWApplicationMyTwitterAPITests : XCTestCase
+
+@end
+
+@implementation TWApplicationMyTwitterAPITests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
